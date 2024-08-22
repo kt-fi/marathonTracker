36d28bc21bb9d319fd53aa47525ac4ee72a2f6c5
Christopher Hillman <kt-five@hotmail.com>
2024-08-22 13:54:43 +0200

    re commit

--- app.js
+++ app.js
@@ -5,22 +5,25 @@ require('dotenv').config()
 
 
 const authRouter = require('./routers/authRouter');
-const { eventNames } = require("./schemas/userSchema");
 
 const app = express();
-
+app.use(function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+   next();
+  });
 app.use(cors());
 app.use(express.json());
 
 app.use("/app", authRouter );
 
 mongoose.set('strictQuery', true);
-mongoose.connect(`mongodb+srv://admin1:rangers20001@cluster0.ra3wn.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`, { 
+mongoose.connect(`mongodb+srv://admin1:${process.env.MONGO_DB_PWD}@cluster0.ra3wn.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`, { 
     //OPTIONS
 })
 
 
 
-app.listen(3001 || process.env.PORT, ()=> {
+app.listen(process.env.PORT, ()=> {
     console.log("Server Started on 3001");
 })
