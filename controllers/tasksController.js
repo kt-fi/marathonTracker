72bb54e032714b053950d8cca7465db320322bb3
Christopher Hillman <kt-five@hotmail.com>
2024-09-11 13:41:54 +0200

    git commit delete multiple docs

--- controllers/tasksController.js
+++ controllers/tasksController.js
@@ -61,69 +61,87 @@ const createTask = async (req, res, next) => {
 
 
 
-const getTasksByUserId = async (req, res, next) => { 
+const getTasksByUserId = async (req, res, next) => {
 
     const userId = req.params.userId;
     let user;
     let tasks;
 
     try {
-        tasks = await User.findOne({userId}).populate('tasksToTrack');
+        tasks = await User.findOne({ userId }).populate('tasksToTrack');
         res.json(tasks.tasksToTrack)
 
-    }catch(err) {
+    } catch (err) {
         const error = new HttpError('Unable to find user', 500)
-        res.json({msg: error.message});
+        res.json({ msg: error.message });
         return next(error)
     }
 };
 
-const getTaskById = async ( req, res, next ) => { 
+const getTaskById = async (req, res, next) => {
 
     const taskId = req.params.taskId;
     let userId;
     let task;
 
     try {
-        
-        task = await Task.findOne({taskId});
+
+        task = await Task.findOne({ taskId });
         res.json(task)
 
-    }catch(err) {
+    } catch (err) {
         const error = new HttpError('Unable to find user', 500)
-        res.json({msg: error.message});
+        res.json({ msg: error.message });
         return next(error)
     }
 }
 
-const editTaskById = async (req, res, next) => { };
+const editTaskById = async (req, res, next) => {
 
-const deleteTaskById = async (req, res, next) => { 
-    console.log('go')
+
+};
+
+const deleteTaskById = async (req, res, next) => {
     let taskId = req.params.taskId;
     let task;
+    let userId;
+
+    let user;
 
     try {
-        task = await Task.findOne({taskId});
-
-        if(!task || task == null || task == undefined) {
-            return res.json({mag: "Task Not Found"})
-        }else {
-            try {
-                // DEKETE FROM USER TASKS LIST //
-                await Task.deleteOne({taskId});
-                res.json({taskId})
-            }catch(err){
-                console.log(err)
-            }
-        }
-    } catch(err) {
-        const error = new HttpError('Task Not Found', 500)
-        res.json({msg: error.message});
-        return next(error)
+        task = await Task.findOne({ taskId });
+    } catch (err) {
+        console.log(err)
     }
 
-  
+    try {
+        if (!task || task == null || task == undefined) {
+            return res.json({ msg: "Task Not Found" })
+        } else {
+            user = await User.findOne({ userId: task.userId })
+        }
+
+    } catch (err) {
+        console.log(err)
+    }
+    try {
+        if (!user || user == null || user == undefined) {
+            return res.json({ msg: "Task Not Found" })
+        } else {
+            let sess = await mongoose.startSession();
+
+            await sess.startTransaction();
+            await Task.deleteOne({ taskId });
+            await user.tasksToTrack.pull(task);
+            await user.save({ session: sess });
+            await sess.commitTransaction();
+        }
+    } catch (err) {
+        const error = new HttpError('Task Not Found', 500)
+        res.json({ msg: error.message });
+        return next(error)
+    }
+    res.json({ taskId })
 };
 
 
@@ -135,7 +153,7 @@ const deleteAllTasks = async (req, res, next) => {
         await Task.deleteMany({})
         await User.deleteMany({});
         res.send('deleted database succesfully')
-    }catch(err) {
+    } catch (err) {
         res.send(err)
     }
 }
